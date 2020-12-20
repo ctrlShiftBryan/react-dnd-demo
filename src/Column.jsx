3dc92a1d33d02ccb40b74f093fad8a12c476de22
Bryan Arendt <bryanarendt@gmail.com>
2020-12-20 08:20:35 -0500

    styles

--- src/Column.jsx
+++ src/Column.jsx
@@ -12,6 +12,8 @@ padding: 8px;
 `
 const TaskList = styled.div`
 padding: 8px;
+transition: background-color 0.2s ease;
+background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')}
 `
 
 export default class Column extends React.Component {
@@ -20,10 +22,11 @@ export default class Column extends React.Component {
       <Container>
         <Title> {this.props.column.title}</Title>
         <Droppable droppableId={this.props.column.id}>
-        {provided => (
+        {(provided, snapshot) => (
           <TaskList 
             ref={provided.innerRef}
             {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
           >
             {this.props.tasks.map((task, index) => <Task key={task.id} task={task} index={index}/>)}
             {provided.placeholder}
--- src/Task.jsx
+++ src/Task.jsx
@@ -9,16 +9,19 @@ border: 1px solid lightgrey;
 padding: 9px;
 margin-bottom: 9px;
 background-color: white;
+transition: background-color 0.2s ease;
+background-color: ${props => props.isDragging ? 'lightgreen' : 'white'}
 `
 
 export default class Task extends React.Component {
   render() {
     return  <Draggable draggableId={this.props.task.id} index={this.props.index}>
          {
-           (provided) => ( <Container
+           (provided, snapshot) => ( <Container
             {...provided.draggableProps} 
             {...provided.dragHandleProps} 
             ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
            > { this.props.task.content } 
            </Container> )
          }
